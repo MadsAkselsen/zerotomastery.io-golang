cd942cb866c712c73068c1e11177fd4ea9e489b9
Mads Akselsen <akselsenmads@gmail.com>
2022-02-13 08:20:41 +0100

    exercises

--- src/lectures/exercise/if-else/if-else.go
+++ src/lectures/exercise/if-else/if-else.go
@@ -47,7 +47,17 @@ func accessDenied() {
 
 func main() {
 	// The day and role. Change these to check your work.
-	today, role := Tuesday, Guest
+	today, role := Sunday, Manager
 
-	accessGranted()
+	if role <= 20 {
+		accessGranted()
+	} else if today >=5 && role == 30 {
+		accessGranted()
+	} else if today <= 4 && role == 40 {
+		accessGranted()
+	} else if (today == 0 || today == 2 || today == 4) && role == 50 {
+		accessGranted()
+	} else {
+		accessDenied()
+	}
 }
