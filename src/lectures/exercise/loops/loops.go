c960739bb0d4165ae3440ea59013215be2600e9a
Mads Akselsen <akselsenmads@gmail.com>
2022-02-13 08:59:16 +0100

    loop exercises

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,18 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 50; i++ {
+
+		divisibleBy3 := i%3 == 0
+		divisibleBy5 := i%5 == 0
+		if divisibleBy3 && divisibleBy5 {
+			fmt.Println("FizzBuzz")
+		}
+		if divisibleBy3 {
+			fmt.Println("Fizz")
+		}
+		if divisibleBy5 {
+			fmt.Println("Buzz")
+		}
+	} 
 }
