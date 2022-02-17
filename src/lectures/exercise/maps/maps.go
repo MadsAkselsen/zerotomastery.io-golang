b48527e5174c63058ace73145d2c7410128baa40
Mads Akselsen <akselsenmads@gmail.com>
2022-02-17 06:32:25 +0100

    maps

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -19,6 +19,31 @@ package main
 
 import "fmt"
 
+func printServerStatus(servers map[string]int) {
+	fmt.Println("Total Servers:", len(servers))
+
+	stats := make(map[int]int)
+
+	for _, status := range servers {
+		switch status {
+		case Online:
+			stats[Online] += 1
+		case Offline:
+			stats[Offline] += 1
+		case Maintenance:
+			stats[Maintenance] += 1
+		case Retired:
+			stats[Retired] += 1
+		default:
+			fmt.Println("Unknown status")
+		}
+	}
+	fmt.Println("Online:", stats[Online])
+	fmt.Println("Offline:", stats[Offline])
+	fmt.Println("Maintenance:", stats[Maintenance])
+	fmt.Println("Retired:", stats[Retired])
+}
+
 const (
 	Online      = 0
 	Offline     = 1
@@ -28,4 +53,27 @@ const (
 
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serversMap := make(map[string]int)
+
+	for _, server := range servers {
+		serversMap[server] = Online
+	}
+
+	fmt.Println("==========")
+	printServerStatus(serversMap)
+
+	serversMap["darkstar"] = Retired
+	serversMap["aiur"] = Offline
+
+	fmt.Println("==========")
+	printServerStatus(serversMap)
+
+	for i := 0; i < len(servers); i++ {
+		serverName := servers[i]
+		serversMap[serverName] = Maintenance
+	}
+
+	fmt.Println("==========")
+	printServerStatus(serversMap)
 }
