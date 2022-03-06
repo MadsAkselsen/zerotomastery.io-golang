278433de5af0acc85031c79f71033c464a2a51ca
Mads Akselsen <akselsenmads@gmail.com>
2022-03-06 10:44:46 +0100

    testing

---
+++ src/lectures/exercise/testing/rcv-func.go
@@ -0,0 +1,79 @@
+//--Summary:
+//  Implement receiver functions to create stat modifications
+//  for a video game character.
+//
+//--Requirements:
+//* Implement a player having the following statistics:
+//  - Health, Max Health
+//  - Energy, Max Energy
+//  - Name
+//* Implement receiver functions to modify the `Health` and `Energy`
+//  statistics of the player.
+//  - Print out the statistic change within each function
+//  - Execute each function at least once
+
+package rcv
+
+// type Health struct {
+// 	health int
+// }
+
+type Player struct {
+	health uint
+	maxHealth uint
+	energy uint
+	maxEnergy uint
+	name string
+}
+
+func (player *Player) IncrementHealth(healthNum int) {
+	health := int(player.health)
+	maxHealth := int(player.maxHealth)
+	if (health + healthNum > maxHealth) {
+		player.health = player.maxHealth
+	} else if (health + healthNum < 0){
+		player.health = 0
+	} else {
+		player.health = uint(health + healthNum)
+	}
+
+	// fmt.Println()
+	// fmt.Println(">>>>", player.name, "<<<<")
+	// fmt.Println("HEALTH:", player.health)
+	// fmt.Println("MAX HEALTH:", player.maxHealth)
+	// fmt.Println("ENERGY:", player.energy)
+	// fmt.Println("MAX ENERY:", player.maxEnergy)
+}
+
+// func (player *Player) incrementEnergy(energyNum int) {
+// 	if (player.energy + energyNum > player.maxEnergy) {
+// 		player.energy = player.maxEnergy
+// 	} else if (player.energy + energyNum < 0){
+// 		player.energy = 0
+// 	} else {
+// 		player.energy = player.energy + energyNum
+// 	}
+
+// 	fmt.Println()
+// 	fmt.Println(">>>>", player.name, "<<<<")
+// 	fmt.Println("HEALTH:", player.health)
+// 	fmt.Println("MAX HEALTH:", player.maxHealth)
+// 	fmt.Println("ENERGY:", player.energy)
+// 	fmt.Println("MAX ENERY:", player.maxEnergy)
+// }
+
+func main() {
+	player1 := Player{
+		health: 10,
+		maxHealth: 10,
+		energy: 5,
+		maxEnergy: 5,
+		name: "Mads",
+	}
+
+	player1.IncrementHealth(-6)
+	player1.IncrementHealth(4)
+
+	// player1.incrementEnergy(-3)
+	// player1.incrementEnergy(7)
+}
--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -1,3 +1,7 @@
+package rcv
+
+import "testing"
+
 //--Summary:
 //  Copy your rcv-func solution to this directory and write unit tests.
 //
@@ -10,3 +14,31 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+//* Write unit tests that ensure:
+//  - Health & energy can not go above their maximums
+//  - Health & energy can not go below 0
+func TestIncrementHealth(t *testing.T) {
+	player1 := Player{
+		health: 10,
+		maxHealth: 10,
+		energy: 5,
+		maxEnergy: 5,
+		name: "Mads",
+	}
+
+	player1.IncrementHealth(1)
+	if player1.health > player1.maxHealth {
+		t.Errorf("health should not exceed maxHealth: %v, want %v", player1.health, player1.maxHealth)
+	}
+
+	player1.IncrementHealth(-12)
+	if player1.health != 0 {
+		t.Errorf("health should be 0 when roming all health: %v, want %v", 0, player1.health)
+	}
+}
+
+
+
+	//* If any of your  tests fail, make the necessary corrections
+	//  in the copy of your rcv-func solution file.
