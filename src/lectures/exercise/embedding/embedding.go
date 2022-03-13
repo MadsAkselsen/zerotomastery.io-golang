c1e54d63f8a984459f694ba8224d5665bb2429ea
Mads Akselsen <akselsenmads@gmail.com>
2022-03-13 13:20:29 +0100

    embedding

--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -23,16 +23,74 @@ type BandwidthUsage struct {
 	amount []Bytes
 }
 
+func (s BandwidthUsage) String() string {
+	return "BandwidthUsage"
+}
+
 type CpuTemp struct {
 	temp []Celcius
+}
+
+func (s CpuTemp) String() string {
+	return "CpuTemp"
 }
 
 type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (s MemoryUsage) String() string {
+	return "MemoryUsage"
+}
+
+func (b BandwidthUsage) CalcAvg() int {
+	var sum Bytes
+	for i := 0; i < len(b.amount); i++ {
+		amount := b.amount[i]	
+		sum += amount
+	}
+
+	return int(sum)/len(b.amount)
+}
+
+func (b CpuTemp) CalcAvg() int {
+	var sum Celcius
+	for i := 0; i < len(b.temp); i++ {
+		amount := b.temp[i]	
+		sum += amount
+	}
+	return int(sum)/len(b.temp)
+}
+
+func (b MemoryUsage) CalcAvg() int {
+	var sum Bytes
+	for i := 0; i < len(b.amount); i++ {
+		amount := b.amount[i]	
+		sum += amount
+	}
+	return int(sum)/len(b.amount)
+}
+
+type Calculator interface {
+	CalcAvg() int
+}
+
+func Calculate(item Calculator) {
+	// fmt.Printf("Avg for v% is %v", item, item.CalcAvg())
+	fmt.Println(item.CalcAvg())
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dashboard := Dashboard{bandwidth, temp, memory}
+	Calculate(dashboard.CpuTemp)
 }
